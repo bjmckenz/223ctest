125c09aacaa80907eae5fbe458be29bb69de1ed8
bjmckenz <>
2022-01-25 21:39:18 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// this is code 
+
+int main(int argc, const char* argv[]) {
+  char buf[1024];
+  char *successful_read;
+  printf("Enter string\n");
+  successful_read = fgets(buf,sizeof(buf),stdin);
+  if (successful_read && strlen(buf) > 0) {
+    buf[strlen(buf)-1] = '\0';
+  }
+  printf("Entered '%s'\n",buf);
+
+  int hh;
+  float mm;
+  if (2 == sscanf(buf,"%d %f",&hh,&mm))
+  {
+    printf("got %d %.2f\n",hh,mm);
+  }
+  return EXIT_SUCCESS;
+}
